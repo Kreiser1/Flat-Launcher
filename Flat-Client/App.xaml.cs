da298acdd28f9bed8ddc39b5f702c611744c42bf
Kreiser1 <kreiser2015@mail.ru>
2026-09-16 11:37:58 +0300

    Добавьте файлы проекта.

---
+++ Flat-Client/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Flat_Client {
+	using System.Configuration;
+	using System.Data;
+	using System.Windows;
+
+	/// <summary>
+	/// Interaction logic for App.xaml
+	/// </summary>
+	public partial class App : Application {
+	}
+
+}

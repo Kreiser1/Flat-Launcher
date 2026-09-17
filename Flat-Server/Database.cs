11b2b3c047f17c2405a6f49919ebc8cb4c54dcf3
Kreiser1 <kreiser2015@mail.ru>
2026-09-17 22:03:20 +0300

    sync

---
+++ Flat-Server/Database.cs
@@ -0,0 +1,28 @@
+﻿namespace Flat_Server {
+	using Dapper;
+	using Npgsql;
+
+	internal static class Database {
+		private static string connectionString = "";
+
+		public static NpgsqlConnection Connection {
+			get {
+				NpgsqlConnection connection = new(connectionString);
+				connection.Open();
+				return connection;
+			}
+		}
+
+		public static void Connect(string connectionString) {
+			Database.connectionString = connectionString;
+
+			try {
+				using var connection = Connection;
+
+				connection.Execute(@"
+					
+				");
+			}
+		}
+	}
+}

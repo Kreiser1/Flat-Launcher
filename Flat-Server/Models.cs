30a33992946518a7e590e00cb2c63960c519b6a1
Kreiser1 <kreiser2015@mail.ru>
2026-09-19 16:23:47 +0300

    database

---
+++ Flat-Server/Models.cs
@@ -0,0 +1,8 @@
+﻿namespace Flat_Server.Database.Models {
+	class User {
+		public required int Id { get; set; }
+		public required string Login { get; set; }
+		public required string PasswordHash { get; set; }
+		public required string Nickname { get; set; }
+	}
+}
